4816dcce65131c4f3547aa6a5aea676e743129a8
mihai133 <mihai.cngm@gmail.com>
2024-04-04 15:09:08 +0300

    Devise setup

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_01_105849) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_04_100237) do
   create_table "friends", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
@@ -21,6 +21,18 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_01_105849) do
     t.datetime "updated_at", null: false
     t.integer "friend_id"
     t.index ["friend_id"], name: "index_friends_on_friend_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "friends", "friends"
